f8358ea2ee1de6ee654c8cf380d4c17a44b66b49
Stela Capsa <stela.capsa@gmail.com>
2019-11-13 21:42:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
+  school_accessories = ["wow", "books", "am", "pencils", "notebook", "calculator", ""]
   
 end
