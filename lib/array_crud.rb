f51767f8678573b601bc9f36bfd52e58306d6b1b
Stela Capsa <stela.capsa@gmail.com>
2019-11-13 21:13:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 def add_element_to_end_of_array(array, element)
     school_accessories = ["books", "pencils", "notebook", "calculator",]
-    school_accessories << "scissor"
+    school_accessories << "arrays!"
   
 end
 
