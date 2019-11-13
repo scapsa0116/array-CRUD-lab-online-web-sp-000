964a63c4124059d8bb01b2eacd567815e109c0b6
Stela Capsa <stela.capsa@gmail.com>
2019-11-13 21:41:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  school_accessories = ["wow", "books", "am", "pencils", "notebook", "calculator",]
+  school_accessories[0]
 end
 
 def retrieve_last_element_from_array(array)
